839274f1eae63cdb8aee64d0316998925c8fc2ea
HolyGrease <kireevdanil2000@gmail.com>
2023-06-25 14:44:12 +0300

    add default values for config parameters

--- src/config.rs
+++ src/config.rs
@@ -10,7 +10,7 @@ pub struct Config {
 
 #[derive(Envconfig)]
 pub struct DiscordConfig {
-    #[envconfig(from = "DISCORD_API_ENDPOINT")]
+    #[envconfig(from = "DISCORD_API_ENDPOINT", default = "https://discord.com/api/channels")]
     pub api_endpoint: String,
     /// Bot auth token
     #[envconfig(from = "DISCORD_TOKEN")]
@@ -22,12 +22,12 @@ pub struct DiscordConfig {
 
 #[derive(Envconfig)]
 pub struct LeetCodeConfig {
-    #[envconfig(from = "LEETCODE_API_ENDPOINT")]
+    #[envconfig(from = "LEETCODE_API_ENDPOINT", default = "https://leetcode.com/graphql")]
     pub api_endpoint: String,
     /// Base url to leetcode to construct url to specific problem
-    #[envconfig(from = "LEETCODE_BASE_URL")]
+    #[envconfig(from = "LEETCODE_BASE_URL", default = "https://leetcode.com")]
     pub base_url: String,
     /// Url to leetcode logo to construct discrod message with
-    #[envconfig(from = "LEETCODE_IMAGE_URL")]
+    #[envconfig(from = "LEETCODE_IMAGE_URL", default = "https://leetcode.com/static/images/LeetCode_logo_rvs.png")]
     pub image_url: String,
 }
